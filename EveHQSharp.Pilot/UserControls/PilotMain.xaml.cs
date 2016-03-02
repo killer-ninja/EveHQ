3d717415eab561af134ba5462849e3a56a5b1999
bonekill <boneki11@hotmail.com>
2016-03-01 19:09:35 -0700

    Prep for plugin system.

--- EveHQSharp.Pilot/UserControls/PilotMain.xaml.cs
+++ EveHQSharp.Pilot/UserControls/PilotMain.xaml.cs
@@ -13,14 +13,14 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace EveHQSharp.PilotManager
+namespace EveHQSharp.Pilot.UserControls
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for PilotMain.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class PilotMain : UserControl
     {
-        public MainWindow()
+        public PilotMain()
         {
             InitializeComponent();
         }
